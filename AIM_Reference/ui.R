1d08c7a54070b3de5d18a0ef753969d674353a1a
Nelson Stauffer <ngs@nmsu.edu>
2016-09-07 13:14:03 -0600

    Set up filtering option interface for terrestrial

--- AIM_Reference/ui.R
+++ AIM_Reference/ui.R
@@ -76,6 +76,19 @@ shinyUI(fluidPage(
                   width='100%'
                   ),
         ## Select terrestrial project(s) ====
+        selectizeInput(inputId = "terrproject", ## This value is used to narrow down the site and ecosite options next
+                       label = "Select a project or projects to filter by.",
+                       choices = unique(tdat.point.fc@data$ProjectName)
+                       ),
+        selectizeInput(inputId = "terrsiteid",
+                       label = "",
+                       choices = list("needs to be populated based on terrproject")
+                       ),
+        selectizeInput(inputId = "terrecosite",
+                       label = "",
+                       choices = list("needs to be populated based on terrproject")
+                       ),
+        ## Display the button to use the entered query ====
         actionButton(inputId = "terragobutton",
                      label="Query TerrADat")
       ) ## Closure for the TerrADat panels
